9dbb3df3ebe8b310d6d106468c97ff26a08053aa
Zhiyuan L <zhiyuan.l@outlook.com>
2019-12-22 22:42:54 +0100

    add comments and improvements

--- Phase 1/converter.sh
+++ Phase 1/converter.sh
@@ -2,12 +2,17 @@
 
 function scan ()
 {
-path=$(cd "$(dirname "$0")"; pwd);
-echo path=$(cd "$(dirname "$0")"; pwd);
+	#display the local path
+	path=$(cd "$(dirname "$0")"; pwd);
+	echo path=$(cd "$(dirname "$0")"; pwd);
+
+	#call the function convert
 	convert;
+
 	filelist=`ls`;
 	for file in $filelist;
 	do
+		#recursive
 		if [ -d $file ];
 		then
 			cd $file;
@@ -16,16 +21,20 @@ echo path=$(cd "$(dirname "$0")"; pwd);
 		fi
 	done
 }
+
 function convert()
 {
 	for im in *.png;
 	do
 		echo "Converting: " $im;
+		#convert png to ppm and rename
 		ffmpeg -i $im $(basename $im png)ppm;
 	done
+	#delete the original function
         rm *.png;
 }
 
 scan
+
 #Before running the program use : chmod +x ./converter.sh
 
